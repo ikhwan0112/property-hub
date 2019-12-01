185b4f51b6fb0f9faf4a17fc9f500e2d04a9b233
Ikhwan Nurhakim <ikhwannurhakim9802@gmail.com>
2019-12-01 21:10:29 +0800

    update model

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'picture', 'phone_no', 'role_id',
+        'name', 'email', 'password', 'picture', 'phone_no', 'is_admin',
     ];
 
     /**
